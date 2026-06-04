5dfffd7d2a60eab71786a4be71a42e3448c34018
Bruce Westbury <brucewestbury@gmail.com>
2026-06-04 09:31:20 +0100

    Add Mathlib dependency

--- lakefile.lean
+++ lakefile.lean
@@ -3,6 +3,9 @@ open Lake DSL
 
 package «certificates» where
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.30.0"
+
 lean_lib «Certificates» where
 
 lean_exe «certificates» where
